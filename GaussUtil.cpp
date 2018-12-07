f66062437c73837ef6affaae61298b9c7a207640
Mateusz Pomykalski <pomykalskimateusz@gmail.com>
2018-12-08 00:03:13 +0100

    Create funcions to resolve gauss algorithm with possibility to choose max value in column

--- GaussUtil.cpp
+++ GaussUtil.cpp
@@ -4,6 +4,75 @@ using namespace std;
 
 GaussUtil::GaussUtil(){}
 
+void GaussUtil::countStandardGauss(double **matrix, int rowSize, int columnSize, int rowIndex, int columnIndex, int stepNumber)
+{
+    double* coefficients = countCoefficients(matrix, rowIndex, columnIndex, rowSize); 
+
+    int coeIndex = 0;
+
+    for(int i = rowIndex + 1; i < rowSize; i++)
+    {
+        modifyRow(matrix, coefficients[coeIndex], i, columnIndex + 1, columnSize, stepNumber);
+        coeIndex++;
+    }
+
+    delete[] coefficients;
+
+    if(stepNumber < rowSize - 1) 
+    {
+        countStandardGauss(matrix, rowSize, columnSize, rowIndex + 1, columnIndex + 1, stepNumber + 1);   
+    }
+}
+
+void GaussUtil::countChooseInColumGauss(MatchematicalMatrix* matchematicalMatrix, double **matrix, int rowSize, int columnSize, int rowIndex, int columnIndex, int stepNumber)
+{
+    double* singleColumn = new double[rowSize - rowIndex];
+    for(int i = 0; i < rowSize; i ++)
+    {
+        singleColumn[i] = matrix[i][columnIndex];
+    }
+    
+    int index = indexOfMaxValue(singleColumn, rowSize - rowIndex);
+
+    delete [] singleColumn;
+
+    if(index != rowIndex)
+    {
+        matchematicalMatrix->exchangeRow(matrix, rowIndex, index);
+    }
+
+    double* coefficients = countCoefficients(matrix, rowIndex, columnIndex, rowSize); 
+
+    int coeIndex = 0;
+
+    for(int i = rowIndex + 1; i < rowSize; i++)
+    {
+        modifyRow(matrix, coefficients[coeIndex], i, columnIndex + 1, columnSize, stepNumber);
+        coeIndex++;
+    }
+
+    delete[] coefficients;
+
+    if(stepNumber < rowSize - 1) 
+    {
+    countChooseInColumGauss(matchematicalMatrix, matrix, rowSize, columnSize, rowIndex + 1, columnIndex + 1, stepNumber + 1);
+    }
+}
+
+int GaussUtil::indexOfMaxValue(double* table, int tableSize)
+{   
+    int indexOfMaxValue = 0;
+    for(int i = 0; i < tableSize; i++)
+    {
+        if(table[i] > table[indexOfMaxValue])
+        {
+            indexOfMaxValue = i;
+        }
+    }
+
+    return indexOfMaxValue;
+}
+
 double* GaussUtil::countCoefficients(double **matrix, int rowIndex, int columnIndex, int rowSize)
 {
     int coefficientsSize = rowSize - (rowIndex + 1);
--- GaussUtil.hpp
+++ GaussUtil.hpp
@@ -1,6 +1,8 @@
 #include <string>
 #include <iostream>
 
+#include "MatchematicalMatrix.hpp"
+
 using namespace std;
 
 class GaussUtil
@@ -8,9 +10,16 @@ class GaussUtil
   public:
     GaussUtil();
 
-    double* countCoefficients(double **matrix, int rowIndex, int columnIndex, int rowSize);
+    void countStandardGauss(double **matrix, int rowSize, int columnSize, int rowIndex, int columnIndex, int stepNumber);
 
+    void countChooseInColumGauss(MatchematicalMatrix* matchematicalMatrix, double **matrix, int rowSize, int columnSize, int rowIndex, int columnIndex, int stepNumber);
+
+  private:
+    double* countCoefficients(double **matrix, int rowIndex, int columnIndex, int rowSize);
+    
     void modifyRow(double** matrix, double coefficient, int startRowIndex, int startColumnIndex, int columnSize, int stepNumber);
+
+    int indexOfMaxValue(double* table, int tableSize);
 
     void printTable(double* table, int size);
 };
